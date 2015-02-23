fe8dcbe8ec59b5aeccdc5723aa23f64e90909662
Sean McMains <sean@mcmains.net>
2015-02-23 13:49:53 -0600

    Set up CocoaPods, added Light Blue Bean library.

---
+++ iOSBluetoothLighting/Pods/Target Support Files/Pods-iOSBluetoothLighting-Bean-iOS-OSX-SDK/Pods-iOSBluetoothLighting-Bean-iOS-OSX-SDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSBluetoothLighting_Bean_iOS_OSX_SDK : NSObject
+@end
+@implementation PodsDummy_Pods_iOSBluetoothLighting_Bean_iOS_OSX_SDK
+@end
---
+++ iOSBluetoothLighting/Pods/Target Support Files/Pods-iOSBluetoothLighting/Pods-iOSBluetoothLighting-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSBluetoothLighting : NSObject
+@end
+@implementation PodsDummy_Pods_iOSBluetoothLighting
+@end
